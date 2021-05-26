4b37a5f5221506bdae298e599205dad578834cf6
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-26 14:26:16 +0530

    store in a dictionary

--- arithmetic_sorting.sh
+++ arithmetic_sorting.sh
@@ -1,14 +1,21 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Arithmetic Computation & Sorting problem "
 read -p "enter a:" a
 read -p "enter b:" b
 read -p "enter c:" c
-echo "user inputs are:$a $b $c"
-#d1=$(($a+$b*$c))
-#echo $d1
-#d2=$(($a*$b+$c))
-#echo $d2
+echo "user inputs are a:$a b:$b c:$c"
+
+declare -A dict
+d1=$(($a+$b*$c))
+echo $d1
+d2=$(($a*$b+$c))
+echo $d2
 d3=$(awk "BEGIN {print ($c+$a/$b)}")
 echo $d3
 d4=$(awk "BEGIN {print ($a%$b+$c)}")
 echo $d4
+dict=([a+b*c]=$d1 [a*b+c]=$d2 [c+a/b]=$d3 [a%b+c]=$d4)
+for key in ${!dict[@]}
+do 
+	echo dict[$key]=${dict[$key]}
+done
