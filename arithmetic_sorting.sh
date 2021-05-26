30d7f50df441207b477290da67fa6bc9217263b6
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-26 13:46:24 +0530

    computing division and add

--- arithmetic_sorting.sh
+++ arithmetic_sorting.sh
@@ -3,6 +3,10 @@ echo "Arithmetic Computation & Sorting problem "
 read -p "enter a:" a
 read -p "enter b:" b
 read -p "enter c:" c
-echo $a $b $c
-d1=$(($a+$b*$c))
-echo $d1
+echo "user inputs are:$a $b $c"
+#d1=$(($a+$b*$c))
+#echo $d1
+#d2=$(($a*$b+$c))
+#echo $d2
+d3=$(($c+$a/$b))
+echo $d3
